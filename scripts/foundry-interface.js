181e3f1f6d96265d8c93825694d6ef2656799c34
Hanna <19157374+hanna-greaves@users.noreply.github.com>
2024-07-13 15:49:10 +0100

    conditional

--- scripts/foundry-interface.js
+++ scripts/foundry-interface.js
@@ -1,7 +1,7 @@
 let foundryInterface = {}
 function buildInterface() { 
     foundryInterface.pathfinderSkills  = CONFIG.PF2E?.skills || CONFIG.PF2E?.skillList
-    foundryInterface.audioHelper = foundry.audio.AudioHelper || AudioHelper
+    foundryInterface.audioHelper = foundry.audio?.AudioHelper || AudioHelper
     foundryInterface.chatMessageTypes = CONST.CHAT_MESSAGE_STYLES || CONST.CHAT_MESSAGE_TYPES
 }
 export { buildInterface, foundryInterface }
